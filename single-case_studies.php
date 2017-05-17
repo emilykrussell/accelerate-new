379ac2d905ef32ee7a9e038f602025696d5f9ddb
Emily Russell <ekluchansky@gmail.com>
2017-05-16 21:49:15 -0400

    advanced custom fields

--- single-case_studies.php
+++ single-case_studies.php
@@ -16,11 +16,51 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
+			<?php while ( have_posts() ) : the_post();
+				$services = get_field('services');
+				$client = get_field('client');
+				$link = get_field('site_link');
+				$image_1 = get_field('image_1');
+				$image_2 = get_field('image_2');
+				$image_3 = get_field('image_3'); 
+				$size = "full"; ?>
+
+			<article class="case-studies">
+				<aside class="case-studies-sidebar">
+					<h2><?php the_title(); ?></h2>
+					<h3><?php echo $services; ?></h3>
+					<h3>Client: <?php echo $client; ?></h3>
+
+					<?php the_content(); ?>
+
+					<p><strong><a href="<?php echo $link; ?>">Site Link</a></strong></p>
+
+				</aside>
+
+				<div class="case-studies-images">
+				<?php if($image_1) { ?>
+					<?php echo wp_get_attachment_image( $image_1, $size ); ?>
+					<?php } ?>
+
+					<?php if($image_2) { ?>
+					<?php echo wp_get_attachment_image( $image_2, $size ); ?>
+					<?php } ?>
+					
+					<?php if($image_3) { ?>
+					<?php echo wp_get_attachment_image( $image_3, $size ); ?>
+					<?php } ?>
+				</div>
+
+				
+				</article>
+
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
+
+
+
+

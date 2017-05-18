5423eff08493530d89a1cefe8c3ae50cb16bb64a
Emily Russell <ekluchansky@gmail.com>
2017-05-17 21:55:54 -0400

    case studies archive

---
+++ archive-case_studies.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The template for displaying the archive of case studies
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<?php while ( have_posts() ) : the_post(); 
+				$size = "full";
+				$services = get_field('services');
+				$client = get_field('client');
+				$link = get_field('site_link');
+				$image_1 = get_field('image_1');  ?>
+
+			<article class="case-studies">
+				<aside class="case-studies-sidebar">
+					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<h3><?php echo $services; ?></h3>
+
+					<?php the_excerpt(); ?>
+
+					<p><strong><a href="<?php the_permalink(); ?>">View Project ></a></strong></p>
+
+				</aside>
+
+				<div class="case-studies-images">
+				<?php if($image_1) { ?>
+				<a href="<?php the_permalink(); ?>">
+					<?php echo wp_get_attachment_image( $image_1, $size ); ?></a>
+					<?php } ?>
+				</div>
+
+				
+				</article>
+
+			<?php endwhile; // end of the loop. ?>
+		<div class="clear-fix"></div>
+		</div><!-- #content -->
+
+	</div><!-- #primary -->
+		
+
+<?php get_footer(); ?>

998b505351df8d5e61b67a243be35a5aaa1898c8
Emily Russell <ekluchansky@gmail.com>
2017-05-24 21:34:09 -0400

    404 page

---
+++ 404.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the 404 error page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="site-content">
+
+		<div id="error404">
+			<h3>Hmmm...is this you right now?</h3>
+			<iframe src="https://giphy.com/embed/3oz8xOWhuVpr3Kw3WE" width="480" height="240" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/vh1-reality-tv-basketball-wives-bbwla-3oz8xOWhuVpr3Kw3WE"></a></p>
+			<h3>Try finding us back over <a href="<?php echo home_url(); ?>">here!</a></h3>
+		</div>
+
+	</div><!-- #primary -->
+
+
+
+
+<?php get_footer(); ?>
